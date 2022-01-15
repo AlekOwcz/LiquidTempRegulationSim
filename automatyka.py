c886ce4b9b2506ed069bf7ab21905d10917beadd
Aleksander Owczarczak <96996586+AlekOwcz@users.noreply.github.com>
2022-01-15 22:36:27 +0100

    Added main project file

---
+++ automatyka.py
@@ -0,0 +1,13 @@
+import math as mt
+from matplotlib import pyplot as plt
+import numpy
+def error():   
+    pass
+
+def signal():
+    pass
+x=[1,2,3]
+y=[1,4,9]
+plt.plot([1,2,3],[1,4,9])
+plt.show()
+
